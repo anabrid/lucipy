28a5e6645f066b238c558bef52013481ead0589d
SvenK <git+sveto@svenk.org>
2024-07-11 15:44:37 +0200

    CI
    I hate it

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_version():
         #return f"{ver}+g{build}"
         return ver
     except (subprocess.CalledProcessError, FileNotFoundError): # no git installed
-        return "v0.1.123+N/A-without-git"
+        return "v0.1.1234"
 
 setuptools.setup(
     name="lucipy",
