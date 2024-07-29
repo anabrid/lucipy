2fc9504902bca948ad87c40a8783cb8818d2c884
SvenK <git+sveto@svenk.org>
2024-07-29 14:33:35 +0200

    Fixing defect global state
    This should not be class attributes but object attributes. I obviously
    was thinking of python dataclasses at the time of writing.

--- lucipy/circuits.py
+++ lucipy/circuits.py
@@ -180,8 +180,8 @@ class MIntBlock:
     """
     Stateful configuration about all the MIntBlock.
     """
-    ics : list[float]
-    k0s : list[int]
+    #ics : list[float]
+    #k0s : list[int]
     
     slow =  1_000
     fast = 10_000
@@ -219,10 +219,10 @@ class Routing:
     generates the Output-centric matrix configuration at the end.
     """
     max_lanes = 32
-    routes : list[Route]
+    #routes : list[Route]
     
     def __repr__(self):
-        return f"Routing({routes})"
+        return f"Routing({self.routes})"
     
     def __init__(self, routes: list[Route] = [], **kwargs):
         super().__init__(**kwargs)  # forwards all unused arguments
