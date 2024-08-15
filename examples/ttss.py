02a119dd4ec524b33e4fac64a5cb8d2f9d8ff447
Bernd Ulmann <ulmann@anabrid.com>
2024-08-15 17:13:16 +0200

    Better implementation of the ttss.

--- examples/ttss.py
+++ examples/ttss.py
@@ -1,3 +1,8 @@
+#
+# This example of a three-time-scale system is due to Christian Kuehn, 
+# "Multiple Time Scale Dynamics", Springer, 2015, p. 418.
+#
+
 from lucipy import Circuit, Simulation
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,7 +17,7 @@ ttss = Circuit()
 
 x     = ttss.int(ic = 1)
 y     = ttss.int()
-z     = ttss.int()
+z     = ttss.int(slow = True)               # Set k_0 = 100 (default is 10^4)
 xx    = ttss.mul()
 xxx   = ttss.mul()
 const = ttss.const()
@@ -24,16 +29,20 @@ ttss.connect(xxx, x, weight = -c3)
 ttss.connect(x, y, weight = epsilon)        # y' = epsilon(x - z)
 ttss.connect(z, y, weight = -epsilon)
 
+# z' = epsilon^2(mu - c1 y), the factor 100 results from k_0 = 100 instead of 
+# 10^4. Effectively the 100 cancels out with epsilon * epsilon, but this 
+# notation makes it possible to vary mu and epsilon without having to rescale 
+# the z integrator.
+ttss.connect(const, z, weight = epsilon * epsilon * mu * 100)
+ttss.connect(y, z, weight = -epsilon * epsilon * c1 * 100)
+
 ttss.connect(x, xx.a, weight = -1)          # xx = x^2
 ttss.connect(x, xx.b)
 
 ttss.connect(xx, xxx.a, weight = -1)        # xxx = x^3
 ttss.connect(x, xxx.b)
 
-# z' = epsilon^2(mu - c1 y)
-ttss.connect(const, z, weight = epsilon * epsilon * mu)
-ttss.connect(y, z, weight = -epsilon * epsilon * c1)
-
+# Run simulation
 sim     = Simulation(ttss)                  # Create simulation object
 t_final = 1000
 
