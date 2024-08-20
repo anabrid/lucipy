04ba747e07008cda0365bd65811317e0786c8def
Bernd Ulmann <ulmann@anabrid.com>
2024-08-20 18:10:47 +0200

    Halvorsen now runs on LUCIDAC rev. 1.

--- examples/halvorsen.py
+++ examples/halvorsen.py
@@ -16,36 +16,36 @@ a  = 1.89
 
 h  = Circuit()
 
-x  = h.int(ic = 0.3)
-y  = h.int(ic = 0.1)
-z  = h.int(ic = 0.3)
+mx  = h.int(ic = -0.3)
+my  = h.int(ic = -0.1)
+mz  = h.int(ic = -0.3)
 x2 = h.mul()
 y2 = h.mul()
 z2 = h.mul()
 
-h.connect(x, x2.a, weight = -1)
-h.connect(x, x2.b)
+h.connect(mx, x2.a)
+h.connect(mx, x2.b)
 
-h.connect(y, y2.a, weight = -1)
-h.connect(y, y2.b)
+h.connect(my, y2.a)
+h.connect(my, y2.b)
 
-h.connect(z, z2.a, weight = -1)
-h.connect(z, z2.b)
+h.connect(mz, z2.a)
+h.connect(mz, z2.b)
 
-h.connect(x,  x, weight = -a / 10)
-h.connect(y,  x, weight = -0.4)
-h.connect(z,  x, weight = -0.4)
-h.connect(y2, x, weight = -1.5)
+h.connect(mx,  mx, weight = a / 10)
+h.connect(my,  mx, weight = 0.4)
+h.connect(mz,  mx, weight = 0.4)
+h.connect(y2, mx, weight = -1.5)
 
-h.connect(y,  y, weight = -a / 10)
-h.connect(z,  y, weight = -0.4)
-h.connect(x,  y, weight = -0.4)
-h.connect(z2, y, weight = -1.5)
+h.connect(my,  my, weight = a / 10)
+h.connect(mz,  my, weight = 0.4)
+h.connect(mx,  my, weight = 0.4)
+h.connect(z2, my, weight = -1.5)
 
-h.connect(z,  z, weight = -a / 10)
-h.connect(x,  z, weight = -0.4)
-h.connect(y,  z, weight = -0.4)
-h.connect(x2, z, weight = -1.5)
+h.connect(mz,  mz, weight = a / 10)
+h.connect(mx,  mz, weight = 0.4)
+h.connect(my,  mz, weight = 0.4)
+h.connect(x2, mz, weight = -1.5)
 
 
 # Run simulation
@@ -59,8 +59,8 @@ result  = sim.solve_ivp(t_final,
                         t_eval = np.linspace(0, t_final, num = 1000000))
 
 # Get data from x- and y-integrator
-x_out, y_out, z_out = result.y[x.id], result.y[y.id], result.y[z.id]
+mx_out, my_out, mz_out = result.y[mx.id], result.y[my.id], result.y[mz.id]
 
-plt.plot(x_out, y_out)              # Create a phase space plot.
+plt.plot(mx_out, my_out)            # Create a phase space plot.
 plt.show()                          # Display the plot.
 
